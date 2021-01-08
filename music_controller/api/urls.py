b18f965748316d9aded83d9f59f3bd55cdf43ec9
Arjun S K <dev.arjunsk@gmail.com>
2021-01-08 15:17:43 +0530

    08-01-21 updated

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-from .views import RoomView
+from .views import RoomView,CreateRoomView
 
 urlpatterns = [
-    path('', RoomView.as_view())
+    path('', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view())
 ]
--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from './Homepage';
-import RoomJoinPage from './RoomJoinPage';
-import CreateRoomPage from './CreateRoomPage';
+
 
 export default class App extends Component{
     constructor(props){
@@ -12,11 +11,9 @@ export default class App extends Component{
         return (
             <div>
                 <HomePage/>
-                <RoomJoinPage/>
-                <CreateRoomPage/>
             </div>
         )
+    }
 }
-
-appDiv = document.getElementById('app');
+const appDiv = document.getElementById('app');
 render(<App name='sk'/>,appDiv);
